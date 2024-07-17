ee31acdf2c62d2352e54f096bb21a64d7cedbb41
Henrique Galieta <hgalieta@gmail.com>
2024-07-17 12:05:40 -0300

    feat: add controller for municipio

---
+++ IluminaRJApi/Controllers/MunicipioController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IluminaRJApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MunicipioController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult PostMunicipio()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        public IActionResult GetMunicipios()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetMunicipioById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
