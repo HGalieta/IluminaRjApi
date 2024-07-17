60bc8df745891d6ba61cd76a5e7a96e320ac6d3c
Henrique Galieta <hgalieta@gmail.com>
2024-07-17 14:13:22 -0300

    feat: add newtonsoft json

--- IluminaRJApi/Program.cs
+++ IluminaRJApi/Program.cs
@@ -10,7 +10,8 @@ builder.Services.AddDbContext<DbContext>(opts =>
 
 builder.Services.AddAutoMapper(AppDomain
     .CurrentDomain.GetAssemblies());
-builder.Services.AddControllers();.AddNewtonsoftJson();
+
+builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
