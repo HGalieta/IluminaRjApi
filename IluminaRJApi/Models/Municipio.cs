1cdae5d9ccdd9c369e7360db5b20c17c186d809c
Henrique Galieta <hgalieta@gmail.com>
2024-07-17 12:55:03 -0300

    fix: alter gastoip property type

--- IluminaRJApi/Models/Municipio.cs
+++ IluminaRJApi/Models/Municipio.cs
@@ -1,11 +1,19 @@
-﻿namespace IluminaRJApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IluminaRJApi.Models
 {
     public class Municipio
     {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "O nome do município deve ser informado.")]
         public string Nome { get; set; }
+        [Required(ErrorMessage = "O total de pontos de luz do município deve ser informado.")]
         public int TotalPontos { get; set; }
+        [Required(ErrorMessage = "O total de pontos de luz com lâmpadas de LED do município deve ser informado.")]
         public int PontosLed { get; set; }
+        [Required(ErrorMessage = "O status de arrecadação de CIP/COSIP pelo município deve ser informado como sim ou não.")]
         public Boolean ArrecadaCip { get; set; }
-        public int GastoIp { get; set; }
+        [Required(ErrorMessage = "O gasto total, em R$, do município com iluminação pública deve ser informado.")]
+        public float GastoIp { get; set; }
     }
 }
