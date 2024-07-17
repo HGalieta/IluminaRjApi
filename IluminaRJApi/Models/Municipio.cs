b723ed4b1a0a578549fbd153a6cd0bfb6c5d29fb
Henrique Galieta <hgalieta@gmail.com>
2024-07-17 12:05:18 -0300

    feat: add model for municipio

---
+++ IluminaRJApi/Models/Municipio.cs
@@ -0,0 +1,11 @@
+﻿namespace IluminaRJApi.Models
+{
+    public class Municipio
+    {
+        public string Nome { get; set; }
+        public int TotalPontos { get; set; }
+        public int PontosLed { get; set; }
+        public Boolean ArrecadaCip { get; set; }
+        public int GastoIp { get; set; }
+    }
+}
